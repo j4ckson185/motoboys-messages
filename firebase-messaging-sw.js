9aa70fe47decac9cbba692ecf492e5a58bb581be
j4ckson185 <jackson_division@hotmail.com>
2024-07-21 15:41:36 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,11 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js')
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
     authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
     projectId: "cabana-8d55e",
     storageBucket: "cabana-8d55e.appspot.com",
     messagingSenderId: "706144237954",
     appId: "1:706144237954:web:345c10370972486afc779b",
-    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com"
+    measurementId: "G-96Y337GYT8"
 };
 
 firebase.initializeApp(firebaseConfig);
