f7ea0940a1cbebb90ac638de142cb55fbdf4d31d
j4ckson185 <jackson_division@hotmail.com>
2024-07-21 15:41:15 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getDatabase, ref, push, onChildAdded, remove } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
@@ -7,11 +6,12 @@ import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/fireb
 const firebaseConfig = {
     apiKey: "AIzaSyB-pF2lRStLTN9Xw9aYQj962qdNFyUXI2E",
     authDomain: "cabana-8d55e.firebaseapp.com",
+    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com",
     projectId: "cabana-8d55e",
     storageBucket: "cabana-8d55e.appspot.com",
     messagingSenderId: "706144237954",
     appId: "1:706144237954:web:345c10370972486afc779b",
-    databaseURL: "https://cabana-8d55e-default-rtdb.firebaseio.com"
+    measurementId: "G-96Y337GYT8"
 };
 
 const app = initializeApp(firebaseConfig);
